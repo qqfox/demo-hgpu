c783fded1fbdc9fb98824e18d97de3598e7e7b07
qqfox <trinhquynhfox@gmail.com>
2022-12-28 03:59:54 +0800

    update

---
+++ main.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 26 18:35:11 2022
+
+@author: Quinn
+"""
+
+import numpy as np 
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import collections
+from collections import defaultdict
+import string
+import re
+import gensim
+import nltk
+nltk.download('punkt')
+from gensim.parsing.preprocessing import STOPWORDS
+lemmatizer=WordNetLemmatizer()
+seed = 42
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from sklearn.decomposition import LatentDirichletAllocation, TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn import metrics
+import pickle
+import warnings
+warnings.filterwarnings("ignore")
+import math
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
+from wordcloud import WordCloud
+import streamlit as st
+from streamlit import components
+
+from nltk.util import ngrams
+import pyLDAvis
+import pyLDAvis.sklearn
+# preprocessing part
+
+nltk.download('punkt')
+from gensim.parsing.preprocessing import STOPWORDS
+
+from preprocessing_data import all_vocab_extraction, word_freq_bigram, preprocessing
+
+header = st.container()
+dataset = st.container()
+features = st.container()
+frequences = st.container()
+lda_model = st.container()
+
+@st.cache 
+def get_data(file_name):  
+    data = pd.read_csv(file_name)
+    return data
+
+
+with header:
+    st.title('Haloo, welcome to Hanxun')
+    st.text('This is a demo for GPU project userface')
+
+with dataset:
+    st.header('this dataset contains all the text crawled from reddit')
+    st.text('dataset description')
+    filename = r"C:\Users\USER\Documents\GitHub\GPU\streamlit\data\data_rx6900_Reddit_070822.csv"
+    text_data = pd.read_csv(filename, index_col = 0)
+
+    # text_data = pd.read_csv(r'C:\Users\USER\Documents\GitHub\GPU\streamlit\data\data_rx6900_Reddit_070822.csv', index_col=0)
+    st.write(text_data.head(5))
+
+    st.text('Separate fist as we need to delete the duplication from posts data. later merge the comment and post text')
+    st.text('comment dataframe columns') 
+    st.write(text_data.columns)
+
+
+with frequences:
+    st.header('Time to train the model')
+    st.text('Here you need to change to parameter of the model, and see if the results are good to capture')
+
+    # text_data['comment_text'] = text_data.comment_text.apply(preprocessing(input_more_sw))
+    text_data['comment_text'] = text_data.comment_text.apply(preprocessing)
+    total_vocabulary = all_vocab_extraction(text_data)
+
+    freq_dist = nltk.FreqDist(total_vocabulary)
+    fig, ax = plt.subplots(figsize=(15,7))
+    freq_dist.plot(25)
+    st.pyplot(fig)
+
+    st.text('frequently mentioned 2 words')
+
+    fig2, ax = plt.subplots(figsize=(15,7))
+    total_vocab_str = " ".join(total_vocabulary)
+    topword = word_freq_bigram(total_vocab_str)
+    topword.plot(25)
+    st.pyplot(fig2)
+
+    wordcloud = WordCloud(
+            background_color='black',
+            stopwords=stopwords,
+            max_words=100,
+            max_font_size=40, 
+            scale=3,
+            random_state=1) # chosen at random by flipping a coin; it was heads
+    wordcloud.generate_from_frequencies(frequencies=freq_dist)
+    fig3 = plt.figure(1, figsize=(20, 16))
+    plt.axis('off')
+    plt.imshow(wordcloud)
+    # plt.savefig('demo_rx6900_Task3_cloud.png', dpi = 900)
+    plt.show()
+    st.pyplot(fig3)
+
+
+with features:
+
+    st.header('the features I created')
+    st.markdown('* **Top frequent words** *')
+    st.markdown('* process text from comments and create features *')
+
+
+
+with lda_model:
+    @st.cache
+
+    def show_topics(vectorizer, lda_model, n_words): #num_words
+        keywords = np.array(vectorizer.get_feature_names())
+        topic_keywords = []
+        for topic_weights in lda_model.components_:
+            top_keyword_locs = (-topic_weights).argsort()[:n_words]
+            topic_keywords.append(keywords.take(top_keyword_locs))
+        return topic_keywords
+
+    sel_col, disp_col = st.columns(2)
+
+    input_more_sw = sel_col.text_input('Please add in words that you want to remove in lower form, for examples', '"hello", "hi"')
+
+    text = [word for word in total_vocabulary if not word in input_more_sw]
+    
+    num_words = sel_col.slider('choose the number of frequent words:', min_value=10, max_value=50,value=10,step=2)
+    num_clusters = sel_col.selectbox('choose the number of clusters', options=[3,4,5,6,7,8],index=0)
+    min_occur = sel_col.selectbox('set the minimum times that words occur in the corpus', options=[3,4,5,6,7,8,9,10],index=2)
+    
+
+    CountVec = CountVectorizer(max_df=0.95, min_df=min_occur, max_features=50000) #min_occur
+    data_vectorized = CountVec.fit_transform(text) # fit input data
+    lda_model_ = LatentDirichletAllocation(n_components=num_clusters, #num_clusters
+                                        max_iter=10, 
+                                        learning_method='online',
+                                        learning_offset=70.,
+                                        learning_decay = .7,
+                                        random_state=0).fit(data_vectorized)
+    topic_keywords = show_topics(CountVec, lda_model_, num_words)    #num_words 10    # number of topic kw can be changed
+
+    disp_col.write(topic_keywords)
+
+    
+    html_ = pyLDAvis.sklearn.prepare(lda_model_, data_vectorized, CountVec)
+    html_string = pyLDAvis.prepared_data_to_html(html_)
+    components.v1.html(html_string, width=1300, height=800, scrolling=True)
+
---
+++ preprocessing_data.py
@@ -0,0 +1,127 @@
+# output return all the vaocabulary from given text data
+
+import numpy as np 
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import collections
+from collections import defaultdict
+import string
+import re
+import gensim
+import nltk
+nltk.download('punkt')
+from gensim.parsing.preprocessing import STOPWORDS
+lemmatizer=WordNetLemmatizer()
+from nltk.util import ngrams
+
+seed = 42
+
+
+# preprocessing part
+
+nltk.download('punkt')
+from gensim.parsing.preprocessing import STOPWORDS
+#Expand the reviews x is aninput string of any length. Convert all the words to lower case
+def preprocessing(text):
+    text = str(text).lower()
+    text = text.replace(",000,000", " m").replace(",000", " k").replace("′", "'").replace("’", "'")\
+                           .replace("won't", " will not").replace("cannot", " can not").replace("can't", " can not")\
+                           .replace("n't", " not").replace("what's", " what is").replace("it's", " it is")\
+                           .replace("'ve", " have").replace("'m", " am").replace("'re", " are")\
+                           .replace("he's", " he is").replace("she's", " she is").replace("'s", " own")\
+                           .replace("%", " percent ").replace("₹", " rupee ").replace("$", " dollar ")\
+                           .replace("€", " euro ").replace("'ll", " will").replace("how's"," how has").replace("y'all"," you all")\
+                           .replace("o'clock"," of the clock").replace("ne'er"," never").replace("let's"," let us")\
+                           .replace("finna"," fixing to").replace("gonna"," going to").replace("gimme"," give me").replace("gotta"," got to").replace("'d"," would")\
+                           .replace("daresn't"," dare not").replace("dasn't"," dare not").replace("e'er"," ever").replace("everyone's"," everyone is")\
+                           .replace("'cause'"," because").replace("i've"," i have").replace("ive"," i have").replace("don't","do not").replace("dont","do not")
+    
+    text = re.sub(r'&lt;', ' ', text) #remove '&lt;' tag
+    text = re.sub(r'[^\x00-\x7f]', ' ', text) #remove non ASCII strings
+    text = re.sub(r'<.*?>', ' ', text) 
+    text = re.sub(r"http\S+", " ", text)
+    text = re.sub(r"www.\S+", " ", text)
+    
+    text = re.sub(r'&lt;', ' ', text) #remove '&lt;' tag
+    text = re.sub(r'<.*?>', ' ', text) # remove html
+    text = re.sub(r'[0-9]+', ' ', text) #remove number
+    text = re.sub(r'[^\w\s]', ' ', text) #remove punctiation    
+    
+    for c in ['\r', '\n', '\t'] :
+        text = re.sub(c, ' ', text) #replace newline and tab with tabs\
+        text = re.sub('\s+', ' ', text) #replace multiple spaces with one space
+    
+    custom_stopword = ['good','better','card','like','right','get','come','want','trying','look','think','need',
+                                            'gpus','subreddit', 'haveseek','haveremaining','ratechapterschaptersdescriptionsdescriptions',
+                                            'came','seeing','mentioned','offering','offered','left','got','said','took', 'selectedcaptionscaptions','havel',
+                                            'ti','looking', 'm','help','tried','know','welcome','potato','u','varies','visiin',
+                                            'comment','buildapcforme','sure','amd','gpu','cpu','having','have',
+                                            'getting','yes','lot','thanks','going','dollar', 'pm']
+    # custom_stopword = custom_stopword.append(new_sw)
+
+    all_stopwords_gensim = STOPWORDS.union(set(custom_stopword))
+    tokens_vs_stop = word_tokenize(text)
+    text = ' '.join([word for word in tokens_vs_stop if not word in all_stopwords_gensim])
+        
+    return text
+
+import spacy
+nlp = spacy.load('en_core_web_lg', disable=['parser', 'ner'])
+
+def posta(texts, allowed_postags=['NOUN', 'VERB']):  # ['NOUN', 'ADJ', 'VERB', 'ADV']
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent))
+        texts_out.append([token for token in doc if token.pos_ in allowed_postags]) # sửa, bỏ lemma đi
+    return texts_out
+
+
+#process post text and title to add more information (need to process separately to avoid duplicate text to cmt)
+
+def all_vocab_extraction(df):
+    # preprocessing the text column in dataframe
+
+    # df['comment_text'] = df.comment_text.apply(preprocessing)
+
+    df = df[:]
+    data_text = df.comment_text.tolist()
+    data_list = list(data_text)
+    data_words = [i.split() for i in data_list]
+
+    pos_text = posta(data_words , allowed_postags=['NOUN', 'VERB'])
+
+    df['pos_comment_text'] = pos_text
+    df['pos_comment_text'] = df['pos_comment_text'].apply(lambda x: ' '.join(map(str, x)))
+
+    corpus =" "
+
+    for i in range(len(df['pos_comment_text'])):
+        corpus= corpus+ ' ' + df['pos_comment_text'].iloc[i]
+        
+    tokens_vs_stop = word_tokenize(corpus)
+    all_text = [word for word in tokens_vs_stop]
+    total_vocabulary = [WordNetLemmatizer().lemmatize(word) for word in all_text]
+
+    return total_vocabulary
+
+def word_freq_bigram(text):
+    
+    _1gram = ''.join(text).split(" ")
+    _2gram = [' '.join(e) for e in ngrams(_1gram, 2)]
+#         _3gram = [' '.join(e) for e in ngrams(_1gram, 3)]
+    word_dist = nltk.FreqDist(_2gram)
+    most_freq_biGram = word_dist.most_common(50)
+        
+    return word_dist
+
+def show_topics(vectorizer, lda_model, n_words=10):
+    keywords = np.array(vectorizer.get_feature_names())
+    topic_keywords = []
+    for topic_weights in lda_model.components_:
+        top_keyword_locs = (-topic_weights).argsort()[:n_words]
+        topic_keywords.append(keywords.take(top_keyword_locs))
+    return topic_keywords
